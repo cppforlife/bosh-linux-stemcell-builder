093ca7cf563136a7c1015a3c0f7c39c3d99a2976
dmitriy kalinin <dkalinin@pivotal.io>
2017-07-19 17:51:17 -0700

    system_users

--- stemcell_builder/stages/system_users/apply.sh
+++ stemcell_builder/stages/system_users/apply.sh
@@ -4,11 +4,5 @@ set -e
 
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
-source $base_dir/lib/prelude_bosh.bash
 
-if [ "${stemcell_operating_system_version}" != "xenial" ]; then
-	run_in_chroot $chroot "
-		/usr/sbin/usermod -L libuuid
-		/usr/sbin/usermod -s /usr/sbin/nologin libuuid
-	"
-fi
+# todo removed users
